a5c084b095140cf50f5263f7916f53880ee85323
Minsoo Kim <dippingsauce101@gmail.com>
2021-12-08 16:03:48 +0900

    봇 관련 토큰값들 Tokens.ini 파일에서 읽어오도록 수정

--- DiscordLostArkBot/Constants/Settings.cs
+++ DiscordLostArkBot/Constants/Settings.cs
@@ -1,10 +1,35 @@
-﻿namespace DiscordLostArkBot.Constants
+﻿using DiscordLostArkBot.Utilities;
+
+namespace DiscordLostArkBot.Constants
 {
     public static class Settings
     {
-        public static string DiscordBotToken = "<Your Discord Token Here>";
-        public static string NotionApiAuthToken = "<Your Token Here>";
-        public static string NotionCalendarDbId = "<Your Calendar DB Id Here>";
-        public static string NotionCalendarUrl = "<Your Notion Calendar URL Here>";
+        public static string DiscordBotToken;
+        public static string NotionApiAuthToken;
+        public static string NotionCalendarDbId;
+        public static string NotionCalendarUrl;
+
+        public static bool Load(IniFile iniFile)
+        {
+            DiscordBotToken = iniFile["Tokens"]["DiscordBotToken"].GetString();
+            NotionCalendarUrl = iniFile["Tokens"]["NotionCalendarUrl"].GetString();
+            NotionApiAuthToken = iniFile["Tokens"]["NotionApiAuthToken"].GetString();
+            NotionCalendarDbId = iniFile["Tokens"]["NotionCalendarDbId"].GetString();
+
+            return AreTokensValid();
+        }
+
+        private static bool AreTokensValid()
+        {
+            if (string.IsNullOrWhiteSpace(DiscordBotToken) ||
+                string.IsNullOrWhiteSpace(NotionApiAuthToken) ||
+                string.IsNullOrWhiteSpace(NotionCalendarDbId) ||
+                string.IsNullOrWhiteSpace(NotionCalendarUrl))
+            {
+                return false;
+            }
+
+            return true;
+        }
     }
 }
--- DiscordLostArkBot/Program.cs
+++ DiscordLostArkBot/Program.cs
@@ -1,6 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using DiscordLostArkBot.Constants;
 using DiscordLostArkBot.Discord;
 using DiscordLostArkBot.Model;
+using DiscordLostArkBot.Utilities;
 
 namespace DiscordLostArkBot
 {
@@ -12,6 +16,19 @@ namespace DiscordLostArkBot
         /// <param name="args"></param>
         private static void Main(string[] args)
         {
+            if (File.Exists("Tokens.ini") == false)
+            {
+                Console.WriteLine($"실행 파일 경로 {AppDomain.CurrentDomain.BaseDirectory}에 Tokens.ini 파일을 생성해 주세요.");
+                return;
+            }
+            IniFile ini = new IniFile();
+            ini.Load("Tokens.ini");
+            if (Settings.Load(ini) == false)
+            {
+                Console.WriteLine($"잘못된 토큰 값이 감지되었습니다. Tokens.ini 파일 내용물을 확인해 주세요.");
+                return;
+            }
+
             new Program().ProgramMain().GetAwaiter().GetResult();
         }
 
